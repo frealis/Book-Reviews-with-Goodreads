279eb177d6e40965636669f38688588fa52a1e9b
frealis <36343653+frealis@users.noreply.github.com>
2019-04-13 12:07:10 -0400

    Add JS to check that passwords match on account creation, add css styles to password match messages, disable register button until passwords match, fixed known issue with bootstrap css 'row' class that adds extra space to ride edge of page

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
 
   console.log('dom content loaded');
+  document.querySelector('.register-button').disabled = true;
 
-  error_login = document.querySelector('.error-login').innerHTML
+  const register_password = document.querySelector('#register_password')
+  const register_password_again = document.querySelector('#register_password_again')
 
-  if (error_login !== '') {
-    // document.querySelector('.error-login').classList.add('alert');
-    // document.querySelector('.error-login').classList.add('alert-danger');
+  register_password.addEventListener('keyup', passwordsMatch);
+  register_password_again.addEventListener('keyup', passwordsMatch);
+
+  function passwordsMatch() {
+    console.log('register_password.value: ', register_password.value)
+    console.log('register_password_again.value: ', register_password_again.value)
+
+    if (register_password.value.length !== 0 && register_password_again.value.length !== 0) {
+      if (register_password.value === register_password_again.value) {
+        document.querySelector('.register-button').disabled = false;
+        document.querySelector('#passwords_match').classList.add('passwords-match')
+        document.querySelector('#passwords_match').classList.remove('passwords-dont-match')
+        document.querySelector('#passwords_match').innerHTML = 'Password match!'
+      } else {
+        document.querySelector('.register-button').disabled = true;
+        document.querySelector('#passwords_match').classList.add('passwords-dont-match')
+        document.querySelector('#passwords_match').classList.remove('passwords-match')
+        document.querySelector('#passwords_match').innerHTML = 'Passwords do not match.'
+      };
+    } else {
+      document.querySelector('#passwords_match').innerHTML = ''
+    };
   }
+
 
 });
